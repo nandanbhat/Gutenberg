6fd52b25b758b4d362608f677b09eec463b81e1a
nandanbhat <bhatnandanmbhat@gmail.com>
2019-08-21 00:49:00 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Button from '@material-ui/core/Button';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <div className='section1' >
+          <h1>Guttenberg Project</h1>
+        </div>
+        <div className="section2">
+          <Button variant="contained" color="primary">
+            Fiction
+          </Button>
+          <Button variant="contained" color="primary" >
+            Drama
+          </Button>
+          <Button variant="contained" color="primary" >
+            Humor
+          </Button>
+          <Button variant="contained" color="primary" >
+            Politics
+          </Button>
+          <Button variant="contained" color="primary" >
+            Philosophy
+          </Button>
+          <Button variant="contained" color="primary" >
+            History
+          </Button>
+          <Button variant="contained" color="primary" >
+            Adventure
+          </Button>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
